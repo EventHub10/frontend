a93d1de99c13e6beed75b479677dcfbe9180c829
Gabriel Ataide <gabriel.ataide07@gmail.com>
2023-07-08 18:10:37 -0300

    feat: signup view and dark theme on components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "antd/dist/antd";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ConfigProvider, theme } from "antd";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
@@ -13,24 +14,31 @@ import EditProfile from "./pages/EditProfile";
 import EditEvent from "./pages/EditEvent";
 
 function App() {
+  const { darkAlgorithm } = theme;
   return (
     <BrowserRouter>
-      <div className="flex flex-col justify-between min-h-screen">
-        <Navbar />
-        <div className="container mx-auto">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/create-event" element={<CreateEvent />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/event-detail" element={<EventDetail />} />
-            <Route path="/edit-profile" element={<EditProfile />} />
-            <Route path="/edit-event" element={<EditEvent />} />
-          </Routes>
+      <ConfigProvider
+        theme={{
+          algorithm: darkAlgorithm,
+        }}
+      >
+        <div className="flex flex-col justify-between min-h-screen">
+          <Navbar />
+          <div className="container mx-auto">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/create-event" element={<CreateEvent />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/event-detail" element={<EventDetail />} />
+              <Route path="/edit-profile" element={<EditProfile />} />
+              <Route path="/edit-event" element={<EditEvent />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </ConfigProvider>
     </BrowserRouter>
   );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,116 @@
+import { Button, Form, Input, message } from "antd";
+import {
+  LockOutlined,
+  UserOutlined,
+  PictureOutlined,
+  MailOutlined,
+} from "@ant-design/icons";
+
 const SignUp = () => {
+  const [form] = Form.useForm();
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const success = () => {
+    messageApi.open({
+      type: "success",
+      content: "Usuário cadastrado",
+      duration: 3,
+    });
+  };
+  const error = () => {
+    messageApi.open({
+      type: "error",
+      content: "Preencha todos os campos do formulário",
+      duration: 3,
+    });
+  };
+
+  const onFinish = (values) => {
+    let formUser = {
+      name: values.name,
+      email: values.email,
+      password: values.password,
+      photo: values.photo,
+      user_events: [],
+    };
+    console.log("user: ", formUser);
+    success();
+    form.resetFields();
+  };
+  const onFinishFailed = (errorInfo) => {
+    error();
+    console.log("Failed:", errorInfo);
+  };
   return (
-    <h1 className="text-6xl p-12 bg-blue-400 text-center">tela de SignUp</h1>
+    <>
+      {contextHolder}
+      <h1 className="text-center text-white text-3xl font-bold px-8">
+        Bem-vindo!
+      </h1>
+      <p className="text-center text-white px-8">
+        Crie sua conta e faça parte da comunidade EventHub.
+      </p>
+      <Form
+        form={form}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        className="max-w-[700px] mx-auto p-8"
+      >
+        <Form.Item
+          name="name"
+          rules={[{ required: true, message: "Coloque o seu nome." }]}
+        >
+          <Input
+            prefix={<UserOutlined className="primary-color" />}
+            type="text"
+            placeholder="Nome"
+            className="custom-input"
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: "Coloque o seu melhor email." }]}
+        >
+          <Input
+            prefix={<MailOutlined className="primary-color" />}
+            type="text"
+            placeholder="Email"
+            className="custom-input"
+          />
+        </Form.Item>
+
+        <Form.Item name="photo" rules={[{ required: false }]}>
+          <Input
+            prefix={<PictureOutlined className="primary-color" />}
+            type="name"
+            placeholder="Foto"
+            className="custom-input"
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Defina sua senha." }]}
+        >
+          <Input
+            prefix={<LockOutlined className="primary-color" />}
+            type="password"
+            placeholder="Senha"
+            className="custom-input"
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button className="primary-button" htmlType="submit" block>
+            CRIAR UMA CONTA
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
